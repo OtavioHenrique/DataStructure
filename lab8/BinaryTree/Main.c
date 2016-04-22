dd6ced5a7ef4d251c317080fb27dadff44d5d5d5
Otavio Henrique <Otavio.henrique@hotmail.com>
2016-04-22 10:29:19 -0300

    Remove Node Method

--- lab8/BinaryTree/Main.c
+++ lab8/BinaryTree/Main.c
@@ -76,7 +76,96 @@ int purge(Tree* __tree, int value){
 	_remove(__tree, search(__tree, value));
 }
 
+int contSuns(Node* __node) {
+
+	int cont;
+
+	if (__node->left != NULL && __node->right != NULL) {
+		cont += 2;
+	} else if (__node->left == NULL && __node->right != NULL) {
+		cont += 1;
+	} else if (__node->left != NULL && __node->right == NULL) {
+		cont += 1;
+	} else {
+		cont += 0;
+	}
+
+	return cont;
+
+}
+
+Node* searchFather (Tree* __tree, int value) {
+
+	Node* now = __tree->root;
+	Node* father = NULL;
+
+	while (now != NULL && now->value != value) {
+		father = now;
+		if(now->value > value)
+			now = now->left;
+		else
+			now = now->right;
+	}
+
+	if (now != NULL && now->value == value)
+		return father;
+	else
+		return NULL;
+
+
+}
+
 int _remove (Tree* __tree, Node* __node) {
+
+	if (__node == NULL)
+		return -1;
+
+	int suns = contSuns(__node);
+	Node* __father = searchFather(__tree, __node->value);
+
+	if (suns == 0) {
+		if (__node->value > __father->value) 
+			__father->right = NULL;
+		else
+			__father->left = NULL;
+		
+		__node == NULL;
+		__tree->total--;
+
+	} else if (suns == 1) {
+		
+		if (__node->value < __father->value){
+			if (__node->left != NULL) 
+				__father->left = __node->left;
+			else
+				__father->left = __node->right;
+		} else {
+			if (__node->left != NULL) 
+				__father->right = __node->left;
+			else
+				__father->right = __node->right;
+		}
+
+		__node = NULL;
+		__tree->total--;
+		
+	} else {
+
+		Node* aux = __node->left;
+
+		if (__node->value < __father->value){
+			__father->left = aux;
+			aux->right = __node->right;
+		} else {
+			__father->right = aux;
+			aux->right = __node->right;
+		} 
+
+	__node = NULL;
+	__tree->total--;
+
+
+	}
 
 }
 
@@ -99,10 +188,20 @@ int main (int argc, char** argv) {
 
 	initTree(&__tree);
     insertTree(&__tree, 5);
+    insertTree(&__tree, 9);
+    insertTree(&__tree, 4);
+    insertTree(&__tree, 13);
     insertTree(&__tree, 8);
+    insertTree(&__tree, 10);
+    insertTree(&__tree, 7);
+    insertTree(&__tree, 2);
     insertTree(&__tree, 1);
     insertTree(&__tree, 3);
-    insertTree(&__tree, 7);
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	preOrder(__tree.root);
+	purge(&__tree, 8);
 	printf("\n");
 	printf("\n");
 	printf("\n");
