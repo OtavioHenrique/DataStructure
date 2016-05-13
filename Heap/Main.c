ee5d5848eff2a913df24d9ce69e8b72d8e638f35
Otavio Henrique <Otavio.henrique@hotmail.com>
2016-05-13 14:24:53 -0300

    Finished

--- Heap/Main.c
+++ Heap/Main.c
@@ -16,45 +16,35 @@ void initializeHeap(heap* h, int n) { //function to initialize the heap
     h->v = malloc(sizeof (int) * n); //allocatin n numbers 
     h->max = n;
     h->total = 0;
-    if (!h->v) {
-        printf("Error\n");
-        system("PAUSE");
-    }
 
+    if (!h->v)
+        printf("Error\n");
+    
 }
 
-void changePosition(heap* h, _bigger, _father){ //function to change the position
+void changePosition(heap* h,int _bigger,int _father){ //function to change the position
 
-    int aux = h->v[_bigger];
+    int aux;
+
+    aux = h->v[_bigger];
 
     h->v[_bigger] = h->v[_father];
-    h->[_father] = aux;
+    h->v[_father] = aux;
 
 }
-
-/**reordena o vetor h->v para que se torne um heap
- */
-void construirHeap(heap* h) { //Transform vector into heap
-	int i;
-	
-	for(i=lastFather(h); i>=0; i--)
-		sift(h,i);
-	
-}
-
 
 void sift(heap* h, int _father) { 
 
-	int leftSon = leftSon(_father);
-    int rightSon = rightSon(_father);
+    int lSon = leftSon(_father);
+    int rSon = rightSon(_father);
     int bigger = _father;
 
 
-    if(h->total < rightSon && rightSon > h->v[bigger])
-        bigger = rightSon;
+    if(h->total > rSon && h->v[rSon] > h->v[bigger])
+        bigger = rSon;
 
-    if(h->total < leftSon && leftSon > h->v[bigger])
-        bigger = leftSon;
+    if(h->total > lSon && h->v[lSon] > h->v[bigger])
+        bigger = lSon;
 
     if(bigger != _father) {
         changePosition(h,bigger,_father);
@@ -63,20 +53,30 @@ void sift(heap* h, int _father) {
 
 }
 
+void buildHeap(heap* h) { //Transform vector into heap
+	int i = 0;
+	
+	for(i=lastFather(h); i>=0; i--)
+		sift(h,i);
+	
+}
+
 void destroiHeap(heap* h) { //Destroys heap
     if (h->v)
         free(h->v);
 }
 
 void insertHeap(heap* h, int _value) {
-	if(h->total == h->max)
+	if(h->total == h->max) {
         printf("The heap is full\n");
+        return;
+    }
 
-    h->v[h->total+1] = _value;
+    h->v[h->total] = _value;
     h->total++;
 
     if(h->total != 1)
-        initializeHeap(h);
+        buildHeap(h);
 
 }
 
@@ -85,28 +85,32 @@ void insertHeap(heap* h, int _value) {
 int removeHeap(heap* h) {
  //troca com o ultimo, da -- e peneira
     int aux = h->v[0];
-    h->[0] = h->v[h->total-1];
+    h->v[0] = h->v[h->total-1];
     h->total--;
-    initializeHeap(h);
+    buildHeap(h);
     return aux;
 }
 
 int main(int argc, char *argv[]) {
 
     heap h;
-    inicializaHeap(&h, 10);
+    initializeHeap(&h, 10);
     insertHeap(&h, 5);
     insertHeap(&h, 6);
     insertHeap(&h, 2);
     insertHeap(&h, 3);
+    insertHeap(&h, 50);
     insertHeap(&h, 1);
+    insertHeap(&h, 0);
+    insertHeap(&h, 13);
+    insertHeap(&h, 44);
 
-    printf("%d", removerMaxHeap(&h));
-    printf("%d", removerMaxHeap(&h));
-    printf("%d", removerMaxHeap(&h));
-    printf("%d", removerMaxHeap(&h));
-    printf("%d", removerMaxHeap(&h));
+    printf("%d\n", removeHeap(&h));
+    printf("%d\n", removeHeap(&h));
+    printf("%d\n", removeHeap(&h));
+    printf("%d\n", removeHeap(&h));
+    printf("%d\n", removeHeap(&h));
+    printf("%d\n", removeHeap(&h));
 
-    system("pause");
     return 0;
 }
